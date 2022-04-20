15fd7e83e6c14db3b7f51756e3f16a99a7f4f531
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-04-20 11:02:07 -0400

    adding updated js functionality to fetch and update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
   
-}
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = event.target[0].value
+        //const input = document.querySelector('input#searchByID');
 
-document.addEventListener('DOMContentLoaded', init);
+
+        console.log(input);
+
+        fetch(`http://localhost:3000/movies/${input}`).then(res => res.json()).
+        then(json => {
+            console.log(json)
+            const h4 = movieDetails.querySelector('h4')
+            const p = movieDetails.querySelector('p')
+            h4.textContent = json.title
+            p.textContent = json.summary
+        })
+
+      });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
